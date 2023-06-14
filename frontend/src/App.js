ac39487bb1129e7310f2cd03f9ebe8edf434967c
Brian <bdbradley00@gmail.com>
2023-06-13 19:58:40 -0800

    Started frontend, starting with login page first

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Login from './pages/login'
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route
+              path = "/"
+              element = {< Login />}
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
