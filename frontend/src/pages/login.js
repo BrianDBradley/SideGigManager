de251c517a5026589461d93396b9c489f6055253
Brian <bdbradley00@gmail.com>
2023-06-14 20:16:53 -0800

    Created basic login logic and page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,9 +1,54 @@
-const login = () => {
+import { useState } from "react"
+
+const Login = () => {
+
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const handleSubmit = async(e) => {
+        e.preventDefault()
+
+        const credentials = {username, password}
+        const response = await fetch('/login', {
+            credentials: "include",
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(credentials)
+        })
+
+        const json = await response.json()
+
+        if(!response.ok) {
+            setError(json.error)
+        }
+        if(response.ok) {
+            setError(null)
+            setUsername('')
+            setPassword('')
+        }
+    }
+
     return (
-        <div className = "login">
+        <form>  
             <h1>Login</h1>
-        </div>
+                <input
+                    type="text"
+                    placeholder="Username"
+                    id="username"
+                    onSubmit={handleSubmit}
+                />
+                <br></br>
+                <input
+                    type="password"
+                    placeholder="password"
+                    id="password"
+                    onSubmit={handleSubmit}
+                />
+                <br></br>
+               <button>Submit</button>
+        </form>
     )
 }
 
-export default login
+export default Login
