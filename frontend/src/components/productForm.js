d3426aab4d68023a84064bd9db3d19af9a52db62
Brian <bdbradley00@gmail.com>
2023-06-29 21:16:03 -0800

    Created start to dropdown menu to select which materials go into products

--- frontend/src/components/productForm.js
+++ frontend/src/components/productForm.js
@@ -1,10 +1,30 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 const ProductForm = () => {
+    const [materials, setMaterials] = useState("")
+    //const [usedMaterials, setUsedMaterials] = useState("")
+
     const [name, setName] = useState("")
     const [costToProduce, setCostToProduce] = useState("")
     const [pricePerUnit, setPricePerUnit] = useState("")
     const [error, setError] = useState(null)
+
+    useEffect(() => {
+        const getMaterials = async () => {
+            const response = await fetch('/control-materials', {
+                method: 'GET'
+            })
+    
+            const json = await response.json()
+    
+            if(response.ok) {
+                setMaterials(json)
+            }
+    
+            // TO-DO : handle unable to retrieve materials
+        }
+        getMaterials()
+    }, [materials])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
@@ -47,25 +67,11 @@ const ProductForm = () => {
 
             <br></br>
 
-            <label>Cost to Produce: </label>
-            <input
-                type="number"
-                onChange={(e)=>setCostToProduce(e.target.value)}
-                id="productionCost"
-            />
-
-            <br></br>
-
-            <label>Price Per Unit: </label>
-            <input
-                type="number"
-                onChange={(e)=>setPricePerUnit(e.target.value)}
-                id="pricePerUnit"
-                step={.01}
-                min={0}
-            />
-
-            <br></br>
+            <select>
+            {materials && materials.map((material) => (
+                    <option value={material.name}>{material.name}</option>
+            ))}
+            </select>
 
             <button>Add Product</button>
             {error && <div className="error">{error}</div>}
